9f4f70059e0a5adff38fe093d2edcba4877a208f
shahzad akram <alyakram786@gmail.com>
2024-01-14 23:58:20 +0500

    functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.getElementById("btnC")
-document.getElementById("btnClearLE")
-document.getElementById("btnClear")
-document.getElementById("")
+let btC = document.getElementById("btnC")
+let btnClearLE = document.getElementById("btnClearLE")
+let btnClear = document.getElementById("btnClear")
+let btnResult = document.getElementById("btnResult")
